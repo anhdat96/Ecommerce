8e5bc8c86db67cf5e8445b54208fb6f02f5bd5b5
anhdat96 <49085300+anhdat96@users.noreply.github.com>
2020-12-08 22:50:17 +0700

    dat organization

--- src/main/java/com/example/demo/models/Orders.java
+++ src/main/java/com/example/demo/models/Orders.java
@@ -3,15 +3,14 @@ package com.example.demo.models;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
 
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.Instant;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-import org.hibernate.annotations.Cache;
-import org.hibernate.annotations.CacheConcurrencyStrategy;
 
 @Entity
 @Table(name = "orders")
