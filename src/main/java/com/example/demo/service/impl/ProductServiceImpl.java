a95ed63a6347810b573227b0d0cb510fbd1afa31
anhdat96 <49085300+anhdat96@users.noreply.github.com>
2020-12-09 09:36:52 +0700

    dat create api

---
+++ src/main/java/com/example/demo/service/impl/ProductServiceImpl.java
@@ -0,0 +1,13 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.service.IProductService;
+import com.example.demo.service.dto.ProductDTO;
+
+import java.util.List;
+
+public class ProductServiceImpl implements IProductService {
+    @Override
+    public List<ProductDTO> getAllProduct() {
+        return null;
+    }
+}
