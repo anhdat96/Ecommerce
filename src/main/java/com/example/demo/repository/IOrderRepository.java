459939dc04298199841daad0b6f73ccd8eaff36b
DustStuart159 <ztrongnguyens96@gmail.com>
2020-12-11 09:57:16 +0700

    Revert "thêm xử lý cho Order"
    This reverts commit 6e33c5754b1e3747a6174e945843376d3dfb8f6e.

--- src/main/java/com/example/demo/repository/IOrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.models.Orders;
-import com.example.demo.models.Products;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IOrderRepository extends JpaRepository<Orders, Long> {
-
-}
