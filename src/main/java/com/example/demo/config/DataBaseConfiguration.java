786e6dd29f39715fbed5dbbca0eb00b9860b89dd
Nguyễn Văn Đạt - CMC Global DU11 <nvdat@ldap.cmcglobal.com.vn>
2020-12-31 11:55:05 +0700

    config réource bundel

--- src/main/java/com/example/demo/config/DataBaseConfiguration.java
+++ src/main/java/com/example/demo/config/DataBaseConfiguration.java
@@ -2,7 +2,10 @@ package com.example.demo.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -14,4 +17,14 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 public class DataBaseConfiguration {
     private final Logger log = LoggerFactory.getLogger(DataBaseConfiguration.class);
+    // this config using to use resource bundle file
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setCacheSeconds(10);
+        messageSource.setBasename("classpath:/i18n/message");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
 }
