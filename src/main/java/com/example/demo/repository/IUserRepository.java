67d94c30140769b044b822b2f7a26fc0ee9aae21
Nguyễn Văn Đạt - CMC Global DU11 <nvdat@ldap.cmcglobal.com.vn>
2021-01-05 17:35:21 +0700

    dat

--- src/main/java/com/example/demo/repository/IUserRepository.java
+++ src/main/java/com/example/demo/repository/IUserRepository.java
@@ -1,16 +1,17 @@
 package com.example.demo.repository;
 
 import com.example.demo.models.User;
-import com.example.demo.repository.custom.IUserRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
-public interface IUserRepository extends JpaRepository<User,Long> , IUserRepositoryCustom {
-    Optional<User> findByUserFirstName( String userFirstName);
+
+public interface IUserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUserFirstName(String userFirstName);
+
     Boolean existsByUserFirstName(String userFirstName);
+
     Boolean existsByUserEmail(String email);
+
     Optional<User> findOneByUserFirstNameIgnoreCase(String userFirstName);
 }
--- src/main/java/com/example/demo/security/UserDetailsServiceImpl.java
+++ src/main/java/com/example/demo/security/UserDetailsServiceImpl.java
@@ -2,17 +2,19 @@ package com.example.demo.security;
 
 import com.example.demo.models.User;
 import com.example.demo.repository.IUserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
+@RequiredArgsConstructor
 @Service
 public class   UserDetailsServiceImpl implements UserDetailsService{
-    @Autowired
-    IUserRepository  iuserRepository;
+    @Qualifier("IUserRepository")
+    private final IUserRepository  iuserRepository;
     @Override
     @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
